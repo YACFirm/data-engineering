ddc1e2f62ffda023f3d26e03e61e10a180d8c221
Geronimo <geronimodeabreu@gmail.com>
2012-12-11 15:28:22 -0430

    Skeleton rails app

---
+++ mongo_challenge/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoChallenge::Application.config.secret_token = '3d4134d46087f2324213eb5cfd588da74173a1c6febf76083f093f25945b259b2d9e602cf59bda7e11965b7cb3dd73a415b39f75e4942d57a2141e314d071fe7'
